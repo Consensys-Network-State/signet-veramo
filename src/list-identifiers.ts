d5a000070a772077dd8df1a1b467430651123e5a
Victor Leipnik <vic@sobol.io>
2025-04-30 22:25:50 -0400

    New tests

--- src/list-identifiers.ts
+++ src/list-identifiers.ts
@@ -1,4 +1,4 @@
-import { agent } from './veramo/setup.js'
+import { agent, privKeyStore } from './veramo/setup.js'
 
 async function main() {
   const identifiers = await agent.didManagerFind()
@@ -6,10 +6,21 @@ async function main() {
   console.log(`There are ${identifiers.length} identifiers`)
 
   if (identifiers.length > 0) {
-    identifiers.map((id) => {
+    for (const id of identifiers) {
       console.log(id)
+      
+      // Get and display private keys for this identifier
+      try {
+        for (const key of id.keys) {
+          const privateKey = await privKeyStore.getKey({ alias: key.kid })
+          console.log('Private Key:', privateKey)
+        }
+      } catch (error) {
+        console.log('Could not get private key:', error.message)
+      }
+      
       console.log('..................')
-    })
+    }
   }
 }
 
